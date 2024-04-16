992dfe49a0f25b3b24fa8afe40bbb163efdd1c50
Kjell Larsson <kjell.m.larsson@se.ibm.com>
2024-04-16 16:29:31 +0200

    updates to reactive messaging

--- src/main/java/com/ibm/ce/reactivemessaging/Consumer.java
+++ src/main/java/com/ibm/ce/reactivemessaging/Consumer.java
@@ -1,8 +1,11 @@
 package com.ibm.ce.reactivemessaging;
 
+import java.util.concurrent.CompletionStage;
+
 import javax.ejb.EJB;
 import javax.enterprise.context.ApplicationScoped;
 
+import org.eclipse.microprofile.reactive.messaging.Acknowledgment;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 
 import com.ibm.ce.jms.Producer;
@@ -13,19 +16,25 @@ public class Consumer {
     //@EJB
     //Producer jmsProducer;
 
-    /**
-     * Consume from DOOR.BADGEIN Kafka Topic
-     */
+    // https://smallrye.io/smallrye-reactive-messaging/4.9.0/kafka/receiving-kafka-records/#acknowledgement
     @Incoming("door")
-    public void consumeDoorEvents(String string) throws Exception {
-        System.out.println("Received door event:  " + string);
-        //jmsProducer.sendMessage(string);
-    }
-
+    @Acknowledgment(Acknowledgment.Strategy.NONE)
+    public void consumeDoorEvents(String string) {
+        System.out.println("Received door event: " + string);  
+        
+        /* 
+        try {
+            jmsProducer.sendMessage(message.getPayload());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }*/
+    } 
+      
     /**
      * Consume from DOOR.BADGEIN Kafka Topic
      */
     @Incoming("order")
+    @Acknowledgment(Acknowledgment.Strategy.NONE)
     public void consumeOrderEvents(String string)  {
         System.out.println("Received order event: " + string);
     }
